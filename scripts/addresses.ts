56b56c803b96094abb7cf0a39b486baaf0d3292f
venuswhispers <venuswhisperer412@gmail.com>
2020-05-20 11:02:12 +0200

    ✨ New addresses for version 0.3.0 (#60)

--- scripts/addresses.ts
+++ scripts/addresses.ts
@@ -6,7 +6,7 @@ export const HOPR_TOKEN: { [key in Networks]: string } = {
   ropsten: undefined,
   rinkeby: undefined,
   goerli: undefined,
-  kovan: '0x30C9071D36E6789516065B25420C9acB54f0EA12',
+  kovan: '0xEC0e1Fc209225CAcA789E6ff8de916484Ed4B3Fd',
   private: '0x66DB78F4ADD912a6Cb92b672Dfa09028ecc3085E',
 }
 
@@ -16,7 +16,7 @@ export const HOPR_CHANNELS: { [key in Networks]: string } = {
   ropsten: undefined,
   rinkeby: undefined,
   goerli: undefined,
-  kovan: '0x248a1152dFBB6a81F8E50b32B11dBE599A6f134b',
+  kovan: '0x41135a6F96e9128107886012F0ea38f8C76475B3',
   private: '0x902602174a9cEb452f60c09043BE5EBC52096200',
 }
 
@@ -26,7 +26,7 @@ export const HOPR_MINTER: { [key in Networks]: string } = {
   ropsten: undefined,
   rinkeby: undefined,
   goerli: undefined,
-  kovan: '0xD12a5A2a64dd3dC6Cb4459C0355a90bB11A5690C',
+  kovan: '0x1565D14C3A2D9984a13008Abdb33a7044b9ad2E0',
   private: '0x0a67180CF519aDF27f1FD32F7255bBa00B536FC6',
 }
 
@@ -36,6 +36,6 @@ export const HOPR_FAUCET: { [key in Networks]: string } = {
   ropsten: undefined,
   rinkeby: undefined,
   goerli: undefined,
-  kovan: '0x84Cf33D7f0C07E34AC267C3BAC49317eBF57e6aD',
+  kovan: '0xD0331e4F21d5CC27c82b1663c8Ead0A9a646b050',
   private: '0x6c97048D67c39ADCe38bbB228fc1bA415fc8f096',
 }
