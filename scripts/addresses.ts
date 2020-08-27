4453b2a0cb9240b1bc1210e2eef8b4ceb2b50a44
venuswhispers <venuswhisperer412@gmail.com>
2020-08-27 12:26:01 +0200

    🐛 update private addresses

--- scripts/addresses.ts
+++ scripts/addresses.ts
@@ -19,7 +19,7 @@ export const HOPR_CHANNELS: { [key in Networks]: string } = {
   rinkeby: undefined,
   goerli: undefined,
   kovan: '0x6eCe0EC9E5F408e664ACc397A8Ac7241841c6658',
-  private: '0x902602174a9cEb452f60c09043BE5EBC52096200',
+  private: '0x0a67180CF519aDF27f1FD32F7255bBa00B536FC6',
   solkol: undefined,
   xdai: '0xf4Ff7E5bf2cC7eF89196c8086982F6f5Ec502685',
 }
@@ -31,7 +31,7 @@ export const HOPR_MINTER: { [key in Networks]: string } = {
   rinkeby: undefined,
   goerli: undefined,
   kovan: undefined,
-  private: '0x0a67180CF519aDF27f1FD32F7255bBa00B536FC6',
+  private: '0x6c97048D67c39ADCe38bbB228fc1bA415fc8f096',
   solkol: undefined,
   xdai: '',
 }
@@ -43,7 +43,7 @@ export const HOPR_FAUCET: { [key in Networks]: string } = {
   rinkeby: undefined,
   goerli: undefined,
   kovan: '0x034869aaF67F09296303D2d42dceEc53F4F04533',
-  private: '0x6c97048D67c39ADCe38bbB228fc1bA415fc8f096',
+  private: '0x2E2c8a6710cb5168ec3362a9c1280E2A1FBf0B5E',
   solkol: undefined,
   xdai: '0xcD1049D51adB3187FB9cc9f786cab99F57eCa4D1',
 }
